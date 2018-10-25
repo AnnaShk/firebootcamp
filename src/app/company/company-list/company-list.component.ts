80f9d0c702e56f5ff56ec934897f76a28185c68b
Anna Shklyarova <anna@cadsharp.com>
2018-10-25 16:01:21 +1100

    Subscription

--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Company } from '../company';
 import { CompanyService } from '../company.service';
 import { tap } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'fbc-company-list',
   templateUrl: './company-list.component.html',
   styleUrls: ['./company-list.component.scss']
 })
-export class CompanyListComponent implements OnInit {
-
+export class CompanyListComponent implements OnInit, OnDestroy {
   companies: Company[] = [];
+  sub: Subscription;
 
   constructor(private companyService: CompanyService) {
   }
 
   ngOnInit() {
-    this.companyService.getCompanies()
+    this.sub = this.companyService.getCompanies()
       .pipe(   // without modifing
         tap(c => console.log('component has companies ', c))  // rxjs operator
       )
@@ -29,5 +30,12 @@ export class CompanyListComponent implements OnInit {
         () => { console.log('COMPLETE'); } // Optional. A handler for the execution-complete notification.
       );
   }
+
+  ngOnDestroy(): void {
+    if (!!this.sub) {
+      this.sub.unsubscribe();
+    }
+  }
+
 
 }
