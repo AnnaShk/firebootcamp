82122e582bc6274a6c34d45745286aa5543148ef
Anna Shklyarova <anna@cadsharp.com>
2018-10-26 09:57:43 +1100

    changeDetection

--- src/app/company/company-table/company-table.component.ts
+++ src/app/company/company-table/company-table.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 import { Company } from '../company';
 
 @Component({
   selector: 'fbc-company-table',
   templateUrl: './company-table.component.html',
-  styleUrls: ['./company-table.component.scss']
+  styleUrls: ['./company-table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush  // 2 sample to catch changes. calls 2 timer
+                                                   // not recommended to use until performance issues in complex app
 })
 export class CompanyTableComponent implements OnInit {
   @Input()
@@ -20,6 +22,11 @@ export class CompanyTableComponent implements OnInit {
 
   deleteCompany(company: Company) {
     this.deleteClicked.emit(company);
+  }
+
+  // 1 sample to catch changes. calls 9 times without changeDetection
+  logChanges() {
+    console.log('Change');
   }
 
 }
