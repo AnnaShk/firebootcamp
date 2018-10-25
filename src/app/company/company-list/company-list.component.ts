2cbbd1978b1a67fdb325d9aa9b9b2113c3e29884
Anna Shklyarova <anna@cadsharp.com>
2018-10-25 16:06:58 +1100

    companies$: Observable and finalize

--- src/app/company/company-list/company-list.component.ts
+++ src/app/company/company-list/company-list.component.ts
@@ -1,40 +1,33 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Company } from '../company';
 import { CompanyService } from '../company.service';
-import { tap, takeWhile } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
+import { tap, finalize } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'fbc-company-list',
   templateUrl: './company-list.component.html',
   styleUrls: ['./company-list.component.scss']
 })
-export class CompanyListComponent implements OnInit, OnDestroy {
-  companies: Company[] = [];
-  componentExist = true;
+export class CompanyListComponent implements OnInit {
+  companies$: Observable<Company[]>;
 
   constructor(private companyService: CompanyService) {
   }
 
   ngOnInit() {
-    this.companyService.getCompanies()
+    this.companies$ = this.companyService.getCompanies()
       .pipe(   // without modifing
-        takeWhile(c => this.componentExist),
-        tap(c => console.log('component has companies ', c))  // rxjs operator
-      )
-      .subscribe(
-        next => {
-          this.companies = next; // Required. A handler for each delivered value. Called zero or more times after execution starts
-          console.log('Got companies');
-        },
-        error => { console.error('Error'); }, // Optional. A handler for an error notification.
-        () => { console.log('COMPLETE'); } // Optional. A handler for the execution-complete notification.
+        tap(c => console.log('component has companies ', c)),  // rxjs operator
+        finalize(() => console.log('COMPLETE'))
       );
+      // .subscribe(
+      //   next => {
+      //     this.companies = next; // Required. A handler for each delivered value. Called zero or more times after execution starts
+      //     console.log('Got companies');
+      //   },
+      //   error => { console.error('Error'); }, // Optional. A handler for an error notification.
+      //   () => { console.log('COMPLETE'); } // Optional. A handler for the execution-complete notification.
+      // );
   }
-
-  ngOnDestroy(): void {
-    this.componentExist = false;
-  }
-
-
 }
