0645d694355cf54d734dc19a323a65506777d4da
Anna Shklyarova <anna@cadsharp.com>
2018-10-25 11:39:25 +1100

    First input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
-@Component({
-  selector: 'fbc-root',
+@Component({   // metadata decorator
+  selector: 'fbc-root',  // markup element to use it in future with fbc prefix
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'firebootcamp-crm';
+  title = 'firebootcamp-crm';  // property
+  constructor() {
+  }
+
+  inputChange(event) {
+    console.log('input changed: ', event);
+    this.title = event.target.value;
+  }
 }
