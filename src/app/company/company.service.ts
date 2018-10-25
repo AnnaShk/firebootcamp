0e8b6e1a6f69924500167be248ebe9e7f43d2d1a
Anna Shklyarova <anna@cadsharp.com>
2018-10-25 15:57:08 +1100

    catch error in server

--- src/app/company/company.service.ts
+++ src/app/company/company.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Company } from './company';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,15 @@ export class CompanyService {
   getCompanies(): Observable<Company[]> {
   return this.httpClient.get<Company[]>(`${this.API_BASE}/company`)
     .pipe(   // without modifing
-      tap(c => console.log('service has companies ', c))
+      tap(c => console.log('service has companies ', c)),
+      catchError(e => this.errorHandler(e))
     );
+  }
+
+  errorHandler(error): Observable<Company[]> {
+    console.log('Error in service', error);
+    // throw error;
+    // if you don't want to throught the exception, you can handle the issue yourself:
+    return new Observable<Company[]>();
   }
 }
