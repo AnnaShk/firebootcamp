394cb28a9260a38da5730481d61172e4fe30d7ed
Anna Shklyarova <anna@cadsharp.com>
2018-10-26 16:25:29 +1100

    Add test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,61 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { of } from 'rxjs';
 
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+let companySvc;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+// Set
+beforeEach(() => {
+  companySvc = {
+    getCompanies: () => of([{
+      name: 'Fake Company',
+      email: 'test@test.com',
+      number: '1234'
+    }])
+  };
+});
+
+describe(`Component: App Component`, () => {
+  // Test 1
+  it('add 1+1', () => {
+    expect(1 + 1).toEqual(2);
   });
 
-  it(`should have as title 'firebootcamp-crm'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('firebootcamp-crm');
+  // Test 2
+  it('Component Title', () => {
+    const component = new AppComponent(null);
+    expect(component.title).toEqual('firebootcamp-crm');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to firebootcamp-crm!');
+  // Test 3
+  it('Component with fake service', () => {
+    const component = new AppComponent(companySvc);
+    component.ngOnInit();
+    component.companyCount$
+      .subscribe(c => {
+        expect(c).toEqual(1);
+      });
+  });
+
+  // Test 4 (with Spy on)
+  it('Component with fake serviceand spy-on', () => {
+    const component = new AppComponent(companySvc);
+    // Spy on
+    spyOn(companySvc, 'getCompanies').and.returnValue(of([
+      {
+        name: 'Fake Company A',
+        email: 'test@test.com',
+        number: '1234'
+      },
+      {
+        name: 'Fake Company B',
+        email: 'test@test.com',
+        number: '1234'
+      }
+    ]));
+    component.ngOnInit();
+    component.companyCount$
+      .subscribe(c => {
+        expect(c).toEqual(2);
+      });
   });
 });
